b310e58bd8ef3e2fc3be387db80fa2fa65a8a7e8
Andy Wang <wang.andy32@gmail.com>
2018-10-07 19:51:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ function distanceFromHqInBlocks(block){
   return Math.abs(42-block);
 }
 
-function distanceFromHqInFeet()
+function distanceFromHqInFeet(block){
+  return distanceFromHqInBlocks(block)*264; 
+}
