0a5205670c72581f2018342b48c035d15b41c133
Andy Wang <wang.andy32@gmail.com>
2018-10-07 19:52:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ function distanceFromHqInBlocks(block){
 function distanceFromHqInFeet(block){
   return distanceFromHqInBlocks(block)*264; 
 }
+
+function distanceTraveledInFeet(block1,block2){
+  
+}
